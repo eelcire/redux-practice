3a2fa07d6ecc31125d61455b9e45b8369ac2201a
bh16113 <eric.lee@macys.com>
2020-07-20 18:15:18 -0400

    fresh page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from "react";
 
-import Header from "./components/Header";
-import FormContainer from "./containers/FormContainer";
-import TodosContainer from "./containers/TodosContainer";
-
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Header />
-        <FormContainer />
-        <TodosContainer />
-      </div>
-    );
+    return <div>I love boobies</div>;
   }
 }
 
--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Button(props) {
-  return <button>Add!</button>;
-}
-
-export default Button;
